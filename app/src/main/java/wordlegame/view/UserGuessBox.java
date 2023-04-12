5c402e5065f2b220a871e7f9fb3adc4c2eb19123
Chloe Biddle <chloebiddle@Chloes-MacBook-Pro.local>
2023-04-12 11:02:44 -0500

    not working jtextfieldwformatter

--- app/src/main/java/wordlegame/view/UserGuessBox.java
+++ app/src/main/java/wordlegame/view/UserGuessBox.java
@@ -2,20 +2,36 @@ package wordlegame.view;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+
 import java.awt.event.*;
+import java.text.ParseException;
 
 public class UserGuessBox extends JPanel
 {
-    private JTextField textBox;
-
+    //private JTextField textBox;
 
     public UserGuessBox()
     {
         //JPanel mainPanel = new JPanel();
         JButton enterButton = new JButton("enter");
-        textBox = new JTextField(5);
-        textBox.setPreferredSize( new Dimension( 5, 30 ) );
-        this.add(textBox);
+        MaskFormatter wordleGuess = null;
+        JFormattedTextField wordleGuessBox = null;
+        try {
+            wordleGuess = new MaskFormatter("UUUUU");
+            wordleGuess.setPlaceholderCharacter('X');
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        //textBox = new JTextField(5);
+        //textBox.setPreferredSize( new Dimension( 5, 30 ) );
+        wordleGuessBox = new JFormattedTextField(wordleGuess);
+        wordleGuessBox.setPreferredSize( new Dimension( 5, 30 ) );
+        wordleGuessBox.setVisible(true);
+        //this.add(textBox);
+        this.add(wordleGuessBox);
         this.add(enterButton);
         this.setPreferredSize(new Dimension(10, 30));
         this.setBackground(Color.BLACK);
