6d3197799847ddcded40ceb4c528fd30c5f7c13c
Amal Rizvi <amal.rizvi@slu.edu>
2023-04-20 21:46:25 -0500

    added more stats, still need to align w main

--- app/src/main/java/wordlegame/view/StatsGUI.java
+++ app/src/main/java/wordlegame/view/StatsGUI.java
@@ -7,11 +7,13 @@ import java.awt.*;
 public class StatsGUI extends JFrame 
 {
     private JLabel gamesWonLabel; 
+    private JLabel totalGamesLabel;
+    private JLabel percentageLabel;
 
     public StatsGUI()
     {
         super("Game Statistics"); 
-        setPreferredSize(new Dimension(300,150));
+        setPreferredSize(new Dimension(300,120));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
@@ -23,15 +25,15 @@ public class StatsGUI extends JFrame
         gamesWonLabel.setFont(new Font("Serif", Font.PLAIN, 20));
         mainPanel.add(gamesWonLabel);
 
-        gamesWonLabel = new JLabel("Total Games Played: 0");
-        gamesWonLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        gamesWonLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        mainPanel.add(gamesWonLabel);
+        totalGamesLabel = new JLabel("Total Games Played: 0");
+        totalGamesLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        totalGamesLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        mainPanel.add(totalGamesLabel);
 
-        gamesWonLabel = new JLabel("Percentage of Wins: 0");
-        gamesWonLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        gamesWonLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        mainPanel.add(gamesWonLabel);
+        percentageLabel = new JLabel("Percentage of Wins: 0");
+        percentageLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        percentageLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        mainPanel.add(percentageLabel);
 
         /* 
         //should we add this?
@@ -48,7 +50,24 @@ public class StatsGUI extends JFrame
 
     public void updateGamesWon(int gamesWon)
     {
-        gamesWonLabel.setText("Games Won: " + gamesWon);
+        gamesWonLabel.setText("Number of Wins: " + gamesWon);
+    }
+
+    public void updateTotalGames(int totalGames)
+    {
+        totalGamesLabel.setText("Total Games Played: " + totalGames);
+    }
+
+    public void updatePercentage(double percentage)
+    {
+        percentageLabel.setText("Percentage of Wins: " + String.format("%.2f", percentage) + "%"); //the .2f is for 2 decimal points btw
+    }
+
+    public void updateStats(int gamesWon, int totalGames, double percentage)
+    {
+        updateGamesWon(gamesWon);
+        updateTotalGames(totalGames);
+        updatePercentage(percentage);
     }
 
 }
